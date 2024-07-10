857e785d66a959ac4e34c4247f3d309ac9b20f37
Shivanshu Karn <karnshivanshu2005@gmail.com>
2024-07-10 21:33:30 +0530

    removed all files from redirector branch

--- app/admin/auth/logout/route.js
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from 'next/navigation'
-
-export async function GET(request) {
-    cookies().delete("adminToken");
-    console.log("Signed out")
-    redirect("/admin/auth/login")
-}
